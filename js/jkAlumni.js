54810e6e06d477ceab01b05241058a6f66d57872
Mradul Verma <mohit00fbd@gmail.com>
2015-06-25 18:06:36 +0530

    Registration Page Added
    Added Signup page, included local jquery file,
    initialized date-picker and file-picker

--- js/jkAlumni.js
+++ js/jkAlumni.js
@@ -2,3 +2,12 @@ $(".button-collapse").sideNav();
 $(document).ready(function(){
       $('.slider').slider({full_width: true});
     });
+
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 40 // Creates a dropdown of 15 years to control year
+  });
+ $(document).ready(function() {
+    $('select').material_select();
+  });
+         
